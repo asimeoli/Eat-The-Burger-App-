71e66723c920cfa34f2059ce8728698a2453e644
asimeoli <asimeoli@gmail.com>
2017-10-01 16:55:36 -0400

    burgers

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+	id INTEGER(10) AUTO_INCREMENT NOT NULL,
+		burger_name VARCHAR(300) NOT NULL,
+		devoured BOOLEAN,
+		date TIMESTAMP,
+		PRIMARY KEY (id)
+);
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mushroom & Swiss Burger", "false");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Blue Burger", "false");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cowboy Burger", "false");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie", "false");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon & Cheddar", "false");
+
+-- CREATE TABLE burgers (
+-- 	id INTEGER(10) AUTO_INCREMENT NOT NULL,
+-- 		burger_name VARCHAR(300) NOT NULL,
+-- 		devoured BOOLEAN,
+-- 		date TIMESTAMP,
+-- 		PRIMARY KEY (id)
+-- );
