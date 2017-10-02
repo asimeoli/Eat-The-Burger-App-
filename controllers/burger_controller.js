9e337626518f333576854e8f9c54296a22624988
asimeoli <asimeoli@gmail.com>
2017-10-02 17:43:19 -0400

    burgers

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -39,13 +39,5 @@ router.put("/:id", function(req, res) {
     });
 });
 
-router.delete("/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-
-    burger.delete(condition, function() {
-        res.redirect("/");
-    });
-});
-
 // Export routes for server.js to use.
 module.exports = router;
